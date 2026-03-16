df6494ea63ad4e1aa18b6109e0a2ca1a7520e436
Galactik-GLK <otto.cyril@outlook.fr>
2026-03-16 14:44:01 +0100

    MAJ ArticleVendu

--- src/main/java/org/eni/encheres/bo/ArticleVendu.java
+++ src/main/java/org/eni/encheres/bo/ArticleVendu.java
@@ -1,5 +1,24 @@
 package org.eni.encheres.bo;
 
-public class ArticleVendu {
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class ArticleVendu {
+    @NotEmpty private int Article;
+    @NotNull private String nomArticle;
+    @NotNull private String description;
+    private LocalDateTime dateDebutEncheres;
+    private LocalDateTime dateFinEncheres;
+    @NotEmpty private int miseAPrix;
+    @NotEmpty private int prixVente;
+    @NotNull private String etatVente;
 }
