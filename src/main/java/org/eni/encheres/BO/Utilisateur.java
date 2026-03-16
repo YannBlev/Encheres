b4e715628e61c6a518cc843d27b64476e7833b48
AlexGit-ENI <alexis.joignant2026@campus-eni.fr>
2026-03-16 14:19:36 +0100

    Add class Utilisateur  v1

---
+++ src/main/java/org/eni/encheres/BO/Utilisateur.java
@@ -0,0 +1,16 @@
+package org.eni.encheres.BO;
+
+public class Utilisateur {
+    private int id;
+    private String pseudo;
+    private String nom;
+    private String prenom;
+    private String email;
+    private String telephone;
+    private String rue;
+    private String codePostal;
+    private String ville;
+    private String motDePasse;
+    private int credit;
+    private boolean administrateur;
+}
