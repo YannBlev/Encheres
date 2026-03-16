1126741c8d54e7e21d9ccca297e61efd4656b60a
julb29 <leblay.julien2026@campus-eni.fr>
2026-03-16 14:56:58 +0100

    Commit ju2

---
+++ src/main/java/org/eni/encheres/BO/Adresse.java
@@ -0,0 +1,23 @@
+package org.eni.encheres.bo;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public class Adresse {
+
+    @NotNull
+    @Size(min = 1, max = 30, message = "La longueur doit être comprise entre 1 et 30 caractères")
+    String rue;
+
+    @NotNull
+    @Size(min = 1, max = 15, message = "La longueur doit être comprise entre 1 et 15 caractères")
+    @Pattern(regexp = "^[0-9]*$", message = "Seuls les chiffres sont autorisés")
+    String code_postal;
+
+    @NotNull
+    @Size (min = 1, max = 30, message = "La longueur doit être comprise entre 1 et 30 caractères")
+    private String ville;
+
+
+}
--- src/main/java/org/eni/encheres/bo/Retrait.java
+++ src/main/java/org/eni/encheres/bo/Retrait.java
@@ -1,9 +1,20 @@
 package org.eni.encheres.bo;
 
-public class Retrait {
-   private int no_article;
-   private String rue;
-   private String code_postal;
-   private String ville;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import static javax.management.Query.value;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Retrait {
+   @NotEmpty private int no_article;
+
+   private Adresse adresse;
 }
