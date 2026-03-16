e20f89daf6fee72d16edbf206ae83104a8fa340d
julb29 <leblay.julien2026@campus-eni.fr>
2026-03-16 16:58:14 +0100

    Commit Ju3

---
+++ src/main/java/org/eni/encheres/bo/Categorie.java
@@ -0,0 +1,14 @@
+package org.eni.encheres.bo;
+
+import org.jspecify.annotations.Nullable;
+
+public class Categorie {
+    private int noCategorie;
+    private String libelle;
+
+    public Categorie(String libelle) {
+        this.libelle = libelle;
+    }
+
+
+}
