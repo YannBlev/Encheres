3610d5b9a77cabc81b2ece01db67d603dc5d89ea
julb29 <leblay.julien2026@campus-eni.fr>
2026-03-17 11:31:17 +0100

    Commit Ju3

--- src/main/java/org/eni/encheres/bo/Utilisateur.java
+++ src/main/java/org/eni/encheres/bo/Utilisateur.java
@@ -13,23 +13,22 @@ import java.util.List;
 @NoArgsConstructor
 public class Utilisateur {
 
-    @NotNull@Min(value = 5)@Max(value = 30)
     private int id;
-    @NotNull@Min(value =3)@Max(value =30)
+    @NotNull@Size(min =3, max=30)
     private String pseudo;
-    @NotNull@Min(value =5)@Max(value =30)
+    @NotNull@Size(min =5, max=30)
     private String nom;
-    @NotNull@Min(value =5)@Max(value =30)
+    @NotNull@Size(min =3, max=30)
     private String prenom;
-    @NotNull@Min(value =10)@Max(value =20)
+    @NotNull
     private String email;
-    @Min(value =5)@Max(value =10)
+    @Size(min =10, max=15)
     private String telephone;
-    @NotNull@Min(value =5)@Max(value =30)
+    @NotNull@Size(min =5, max=30)
     private String rue;
     @Pattern(regexp = "\\d{5}")@NotNull@Digits(integer = 5, fraction = 0, message = "Le code postal doit comporter 5 chiffres")
     private String codePostal;
-    @NotNull@Min(value =5)@Max(value =30)
+    @NotNull@Size(min =3, max=30)
     private String ville;
     @Pattern(regexp = "\\b[A-Z0-9._%-]+@[A-Z0-9.-]+\\.[A-Z]{2,4}\\b")@NotNull@Min(value =5)@Max(value =30, message = "Le mot de passe doit comporter une suite Alphanumérique")
     private String motDePasse;
