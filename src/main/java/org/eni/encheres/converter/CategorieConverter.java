dccc4f5795c016b2354f5d1927fc967871f816a1
YannBlev <36kbrl@gmail.com>
2026-03-18 15:43:23 +0100

    Ajout CategorieRowMapper pour afficher les id

--- src/main/java/org/eni/encheres/converter/CategorieConverter.java
+++ src/main/java/org/eni/encheres/converter/CategorieConverter.java
@@ -6,21 +6,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
-import static org.apache.tomcat.util.http.parser.Host.parse;
-
 @Component
 public class CategorieConverter implements Converter<String, Categorie> {
-
 
     @Autowired
     private CategorieService categorieService;
 
     @Override
     public Categorie convert(String idFormatTexte) {
-        // étape 1 : convertir le texte en nombre
-        int id = Integer.parseInt(idFormatTexte); // parseLong transforme du texte à son équivalent au format long
-
-        // étape 2 : retourner le genre correspondant à l'id
+        int id = Integer.parseInt(idFormatTexte);
         return categorieService.consulterCategorieParId(id);
     }
 
