b36157a876f25bd44151ce0e66eecbc22ec69da2
Galactik-GLK <otto.cyril@outlook.fr>
2026-03-19 15:42:58 +0100

    MAJ enchere controller

--- src/main/java/org/eni/encheres/controller/EnchereController.java
+++ src/main/java/org/eni/encheres/controller/EnchereController.java
@@ -2,7 +2,9 @@ package org.eni.encheres.controller;
 
 
 import org.eni.encheres.bo.Enchere;
+import org.eni.encheres.dal.ArticleVenduDao;
 import org.eni.encheres.dal.EnchereDao;
+import org.eni.encheres.service.ArticleVenduService;
 import org.eni.encheres.service.EnchereService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -17,6 +19,8 @@ import java.util.List;
 public class EnchereController {
     @Autowired
     private EnchereService enchereService;
+    @Autowired
+    private ArticleVenduService articleVenduService;
 
     @ModelAttribute("encheres")
     public List<Enchere> getAttributeModelEncheres() {
@@ -34,12 +38,12 @@ public class EnchereController {
     model.addAttribute("enchere",new Enchere());
         return "page/nouvelleVente";
     }
-//    @PostMapping("/{nouvelleVente}")
-//    public String getNouvelleVente(@PathVariable String nouvelleVente, Model model){
-//        model.addAttribute("encheres", enchereService.consulterEncheresParId(nouvelleVente));
-//        return "nouvelleVente";
-//        }
-//    }
+    @PostMapping("/nouvelleVente")
+    public String getNouvelleVente(@PathVariable String nouvelleVente, Model model){
+        model.addAttribute("nouvelleVente", articleVenduService.listArticlesVendu().get(Integer.parseInt(nouvelleVente)));
+        return "page/nouvelleVente";
+        }
+
 
     @GetMapping("/{id}/detail")
     public String detail(@PathVariable("id") int id){
