8f2a3caab71b9fef49d4a30f1f79e91442a70c60
YannBlev <36kbrl@gmail.com>
2026-03-19 16:04:20 +0100

    MAJ RowMapper

--- src/main/java/org/eni/encheres/controller/EnchereController.java
+++ src/main/java/org/eni/encheres/controller/EnchereController.java
@@ -1,10 +1,13 @@
 package org.eni.encheres.controller;
 
 
+import lombok.AllArgsConstructor;
+import org.eni.encheres.bo.ArticleVendu;
 import org.eni.encheres.bo.Enchere;
 import org.eni.encheres.dal.ArticleVenduDao;
 import org.eni.encheres.dal.EnchereDao;
 import org.eni.encheres.service.ArticleVenduService;
+import org.eni.encheres.service.CategorieService;
 import org.eni.encheres.service.EnchereService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -14,12 +17,12 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
+@AllArgsConstructor
 @RequestMapping("/encheres")
-
 public class EnchereController {
-    @Autowired
+
     private EnchereService enchereService;
-    @Autowired
+    private CategorieService categorieService;
     private ArticleVenduService articleVenduService;
 
     @ModelAttribute("encheres")
@@ -35,7 +38,9 @@ public class EnchereController {
 
     @GetMapping("/nouvelleVente")
     public String nouvelleVente(Model model){
-    model.addAttribute("enchere",new Enchere());
+        model.addAttribute("categories", categorieService.consulterCategorie());
+        model.addAttribute("article",new ArticleVendu());
+
         return "page/nouvelleVente";
     }
     @PostMapping("/nouvelleVente")
