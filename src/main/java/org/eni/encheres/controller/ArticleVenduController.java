de413433c16375ad3c06b9ced6dd3cc462626e74
YannBlev <36kbrl@gmail.com>
2026-03-27 12:49:37 +0100

    Ajout des controles du formulaire nouvelleVente.html dans ArticleVenduController

--- src/main/java/org/eni/encheres/controller/ArticleVenduController.java
+++ src/main/java/org/eni/encheres/controller/ArticleVenduController.java
@@ -12,12 +12,15 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 
 @Controller
@@ -37,6 +40,8 @@ public class ArticleVenduController {
 
     @GetMapping("/profil/{id}/nouvelleVente")
     public String getNouvelleVente(@PathVariable int id, Model model){
+        model.addAttribute("dateMinDebut", LocalDate.now());
+        model.addAttribute("dateMinFin", LocalDate.now().plusDays(1));
         model.addAttribute("utilisateur", utilisateurService.getUtilisateurById(id));
         model.addAttribute("articleDto",new ArticleDto());
 
@@ -44,7 +49,7 @@ public class ArticleVenduController {
     }
 
     @PostMapping("/profil/{id}/nouvelleVente")
-    public String postNouvelleVente(@PathVariable int id, @RequestParam("image") MultipartFile file, ArticleDto articleDto, BindingResult bindingResult) throws IOException {
+    public String postNouvelleVente(@PathVariable int id, @RequestParam("image") MultipartFile file, ArticleDto articleDto, BindingResult bindingResult, RedirectAttributes redirectAttributes) throws IOException {
 //        if (bindingResult.hasErrors()) {
 //            return "index";
 //        }
@@ -55,6 +60,27 @@ public class ArticleVenduController {
         if (!file.isEmpty()) {
             String imagePath = fileArticleService.save(file);
             articleDto.setImagePath(imagePath);
+        }
+
+        /**
+         * S'assurer que la date du début de l'enchere ne soit pas inférieur à la date d'aujourd'hui
+         */
+        if (articleDto.getDateDebutEncheres().isBefore(LocalDate.now())) {
+            redirectAttributes.addFlashAttribute("messageErreur", "La date du début d'enchere ne doit pas être inférieur à aujourd'hui.");
+            return "redirect:/encheres/profil/" + id + "/nouvelleVente";
+        }
+
+        /**
+         * S'assurer que la date de fin de l'enchere soit supérieur à la date d'aujourd'hui
+         */
+        if (articleDto.getDateFinEncheres().isBefore(LocalDate.now().plusDays(1))) {
+            redirectAttributes.addFlashAttribute("messageErreur", "La date de fin de l'enchere doit être supérieur à aujourd'hui.");
+            return "redirect:/encheres/profil/" + id + "/nouvelleVente";
+        }
+
+        if (articleDto.getPrixInitial()<10) {
+            redirectAttributes.addFlashAttribute("messageErreur", "Le prix initial doit être supérieur à 10 crédits.");
+            return "redirect:/encheres/profil/" + id + "/nouvelleVente";
         }
 
         /**
